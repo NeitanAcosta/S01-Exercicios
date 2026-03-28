aad8d8f880509f120294ce2fa6416324222b72b5
Nathan Arruola <163927856+NeitanAcosta@users.noreply.github.com>
2026-03-27 22:20:53 -0300

    Relatório 04 -Exercicio 2

---
+++ Relatório 04 -Exercicio 2.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+
+	fmt.Print("Digite os 3 lados do triângulo: ")
+	fmt.Scan(&a, &b, &c)
+
+	// Verifica se forma triângulo
+	if a+b > c && a+c > b && b+c > a {
+
+		switch {
+		case a == b && b == c:
+			fmt.Println("Triângulo Equilátero")
+		case a == b || a == c || b == c:
+			fmt.Println("Triângulo Isósceles")
+		default:
+			fmt.Println("Triângulo Escaleno")
+		}
+
+	} else {
+		fmt.Println("Erro: Os valores não formam um triângulo")
+	}
+}
