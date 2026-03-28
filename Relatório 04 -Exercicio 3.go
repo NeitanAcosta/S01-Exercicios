070e104ba3a9f37c66c67cbeba4583afde67ac34
Nathan Arruola <163927856+NeitanAcosta@users.noreply.github.com>
2026-03-27 22:21:32 -0300

    Relatório 04 -Exercicio 3

---
+++ Relatório 04 -Exercicio 3.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) {
+	a, b := 0, 1
+
+	for i := 0; i < n; i++ {
+		fmt.Print(a, " ")
+		a, b = b, a+b
+	}
+	fmt.Println()
+}
+
+func main() {
+	var n int
+
+	fmt.Print("Quantos números da sequência Fibonacci? ")
+	fmt.Scan(&n)
+
+	fibonacci(n)
+}
