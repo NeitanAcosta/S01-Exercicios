b6457616b5f22f9413e19e08bc04c935c7489cd7
Nathan Arruola <163927856+NeitanAcosta@users.noreply.github.com>
2026-03-30 18:43:57 -0300

    Relatório 5 - Exercicio 2

---
+++ Relatório5-Exercicio2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+float multiplicaArray(float arr[], int tamanho) {
+    float produto = 1;
+
+    for (int i = 0; i < tamanho; i++) {
+        produto *= arr[i];
+    }
+
+    return produto;
+}
+
+int main() {
+    int tamanho;
+
+    cout << "Quantos valores deseja inserir? ";
+    cin >> tamanho;
+
+    float arr[tamanho];
+
+    for (int i = 0; i < tamanho; i++) {
+        cout << "Digite o valor " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Produto dos valores: " << multiplicaArray(arr, tamanho) << endl;
+
+    return 0;
+}
